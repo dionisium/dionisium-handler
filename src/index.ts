50eec5afdac80e8154d808d24582021e5206a48b
ezequiel sañico <exequiel0753@gmail.com>
2023-07-02 02:13:50 -0300

    buildv

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import mercurius from 'mercurius';
 
 async function start():Promise<void>{
     const app = fastify({logger:true});
-    await app.register(cors);
+    await app.register(cors, {origin:'https://dionisium.vercel.app'});
 
     // DATABASE
     require('./database');
