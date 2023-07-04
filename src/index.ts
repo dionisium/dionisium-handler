580e69c41fd1281821ac5c8786c052e46248e5a7
ezequiel sañico <exequiel0753@gmail.com>
2023-07-04 05:40:11 -0300

    cors origin

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import mercurius from 'mercurius';
 
 async function start():Promise<void>{
     const app = fastify({logger:true});
-    await app.register(cors, {credentials:false});
+    await app.register(cors, {origin:/\.dionisium\.vercel\.app$/, credentials:false});
 
     // DATABASE
     require('./database');
