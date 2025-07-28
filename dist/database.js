1d9c655c41bc0c3d45d32e4cf77e0802c65eea84
alex tomura <90726193+alex-tomura@users.noreply.github.com>
2025-07-28 02:54:49 -0300

    Delete dist directory

--- dist/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.URI_MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('database on connection'))
-    .catch(err => console.error(err));
-//# sourceMappingURL=database.js.map
